7d5bc82d86bb265d697afe82f8c06eaed44dbc44
gutianci <gutianci@pwrd.com>
2020-03-09 17:24:11 +0800

    test: cli initdb, import, export

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,5 @@
 import unittest
+from pathlib import Path
 
 from click.testing import CliRunner
 
@@ -12,3 +13,66 @@ class TestGeneralOptions(unittest.TestCase):
         result = runner.invoke(cli, ['--version'])
         self.assertEqual(result.exit_code, 0)
         self.assertIn(__version__, result.output)
+
+
+class TestInitdb(unittest.TestCase):
+    def test_normal(self):
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            result = runner.invoke(cli, ['initdb', 'testdb.sqlite'])
+            self.assertEqual(result.exit_code, 0)
+    
+    def test_db_already_exists(self):
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            Path('testdb.sqlite').touch()
+            result = runner.invoke(cli, ['initdb', 'testdb.sqlite'])
+            self.assertEqual(result.exit_code, 0)
+            self.assertIn('Error', result.output)
+
+
+class TestImport(unittest.TestCase):
+    def test_import_file_normal(self):
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            runner.invoke(cli, ['initdb', 'testdb.sqlite'])
+            Path('testimage.jpg').touch()
+            result = runner.invoke(cli, ['import', 'testimage.jpg', 'testdb.sqlite'])
+            self.assertEqual(result.exit_code, 0)
+            self.assertIn('done', result.output)
+
+    def test_import_dir_normal(self):
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            runner.invoke(cli, ['initdb', 'testdb.sqlite'])
+            Path('testdir').mkdir()
+            Path('testdir/img1.jpg').touch()
+            Path('testdir/img2.jpg').touch()
+            result = runner.invoke(cli, ['import', 'testdir', 'testdb.sqlite'])
+            self.assertEqual(result.exit_code, 0)
+    
+    def test_import_src_not_exists(self):
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            runner.invoke(cli, ['initdb', 'testdb.sqlite'])
+            result = runner.invoke(cli, ['import', 'notexistsSrc', 'testdb.sqlite'])
+            self.assertEqual(result.exit_code, 0)
+            self.assertIn('Error', result.output)
+    
+    def test_import_db_not_exists(self):
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            Path('testimage.jpg').touch()
+            result = runner.invoke(cli, ['import', 'testimage.jpg', 'testdb.sqlite'])
+            self.assertEqual(result.exit_code, 0)
+            self.assertIn('Error', result.output)
+
+
+class TestExport(unittest.TestCase):
+    def test_normal(self):
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            runner.invoke(cli, ['initdb', 'testdb.sqlite'])
+            Path('testdir').mkdir()
+            result = runner.invoke(cli, ['export', 'testdb.sqlite', 'testdir'])
+            self.assertEqual(result.exit_code, 0)
